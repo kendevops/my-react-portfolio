ca18cf8ec24c6794c3ba59f832145b2b3b08d4b9
Kenneth <kendevops@gmail.com>
2021-07-22 07:26:51 +0100

    created basic structure of the contact page

--- src/Components/ContactSection.js
+++ src/Components/ContactSection.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
-function ContactSection() {
-    return (
-        <div className="Contact">
-
-        </div>
-    )
+function ContactSection({ icon, text1, text2, title }) {
+  return (
+    <div className="contact">
+      <img src={icon} alt="" />
+      <div className="right-items">
+        <h6>{title}</h6>
+        <p>{text1}</p>
+        <p>{text2}</p>
+      </div>
+    </div>
+  );
 }
 
-export default ContactSection
+export default ContactSection;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,7 +4,7 @@ import ContactSection from "../Components/contactSection";
 
 const Contact = () => {
   return (
-    <div className="Contact">
+    <div className="Contacts">
       <Title title={"Contact"} span={"Contact"} />
       <div className="map-sect"></div>
       <div className="contact-sect">
@@ -12,16 +12,19 @@ const Contact = () => {
           icon={phone}
           text1={"+2347033960652"}
           text2={"(+234) 815 2269 553"}
+          title={"Phone"}
         />
         <ContactSection
           icon={email}
           text1={"+2347033960652"}
           text2={"(+234) 815 2269 553"}
+          title={"Email"}
         />
         <ContactSection
           icon={location}
           text1={"+2347033960652"}
           text2={"(+234) 815 2269 553"}
+          title={"Location"}
         />
       </div>
     </div>
