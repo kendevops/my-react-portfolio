f43f1b6887194074f7136d6cd3b6d788ee45c6e0
Kenneth <kendevops@gmail.com>
2021-07-22 07:41:49 +0100

    added more properties to the contact list

--- src/Components/ContactSection.js
+++ src/Components/ContactSection.js
@@ -2,12 +2,14 @@ import React from "react";
 
 function ContactSection({ icon, text1, text2, title }) {
   return (
-    <div className="contact">
-      <img src={icon} alt="" />
-      <div className="right-items">
-        <h6>{title}</h6>
-        <p>{text1}</p>
-        <p>{text2}</p>
+    <div className="ContactSection">
+      <div className="contact">
+        <img src={icon} alt="" />
+        <div className="right-items">
+          <h6>{title}</h6>
+          <p>{text1}</p>
+          <p>{text2}</p>
+        </div>
       </div>
     </div>
   );
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,22 +6,33 @@ const Contact = () => {
   return (
     <div className="Contacts">
       <Title title={"Contact"} span={"Contact"} />
-      <div className="map-sect"></div>
+      <div className="map-sect">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d147665.4209188347!2d6.956277835259235!3d4.831198917144095!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1069cea39f2c48e3%3A0x53562bdd7d8832db!2sPort%20Harcourt!5e0!3m2!1sen!2sng!4v1626935573016!5m2!1sen!2sng"
+          title="map"
+          width="600"
+          height="450"
+          style={{ border: 0 }}
+          allowFullScreen=""
+          loading="lazy"
+          tabIndex="0"
+        ></iframe>
+      </div>
       <div className="contact-sect">
         <ContactSection
-          icon={phone}
+          // icon={phone}
           text1={"+2347033960652"}
           text2={"(+234) 815 2269 553"}
           title={"Phone"}
         />
         <ContactSection
-          icon={email}
+          // icon={email}
           text1={"+2347033960652"}
           text2={"(+234) 815 2269 553"}
           title={"Email"}
         />
         <ContactSection
-          icon={location}
+          // icon={location}
           text1={"+2347033960652"}
           text2={"(+234) 815 2269 553"}
           title={"Location"}
