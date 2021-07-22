c392556715ce98daa905ac938fb6344459ad7456
Kenneth <kenneth.aladi@xische.com>
2021-07-22 17:07:59 +0100

    Added some changes to the content

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ import ImageSection from "../Components/ImageSection";
 import ServiceSection from "../Components/ServiceSection";
 import SkillsSection from "../Components/SkillsSection";
 import Title from "../Components/Title";
-// import img from "../img/shineBg.jpeg";
+import cloud from "../img/cloud.svg";
 import design from "../img/design3.png";
 
 const About = () => {
@@ -28,7 +28,7 @@ const About = () => {
       <div className="services-container">
         <ServiceSection image={design} title={"Front-End Developer"} description={"I Develop user-friendly features using uptodate technologies, I also build reusable code and libraries for future use, I ensure the technical feasibility of UI/UX designs are well implimented, Optimize application for maximum speed and scalability."} />
         <ServiceSection image={design} title={"FACILITATOR/MENTOR"} description={"I am a Facilitator and Mentor, I have on different occassion facilitated for Google African Developer Scholarship, Facebook Developer Circle Training and also the Lead Facilitator to Code2Teach Africa, I am a passionate tech enthusiast, I teach programming with passion and i ensure i explain in details concepts about programming to my students."} />
-        <ServiceSection image={design} title={"Front-End Web Developer"} description={"I Develop user-friendly features using uptodate technologies, I also build reusable code and libraries for future use, I ensure the technical feasibility of UI/UX designs are well implimented, Optimize application for maximum speed and scalability."} />
+        <ServiceSection image={cloud} title={"Cloud Developer"} description={"I Develop user-friendly features using uptodate technologies, I also build reusable code and libraries for future use, I ensure the technical feasibility of UI/UX designs are well implimented, Optimize application for maximum speed and scalability."} />
       </div>
     </div>
   );
