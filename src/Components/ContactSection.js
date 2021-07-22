2f67ae7de0e2746dbe8e0d3ea1442c964b44e633
Kenneth <kendevops@gmail.com>
2021-07-22 07:22:55 +0100

    added contact page render

---
+++ src/Components/ContactSection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ContactSection() {
+    return (
+        <div className="Contact">
+
+        </div>
+    )
+}
+
+export default ContactSection
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,29 @@
 import React from "react";
 import Title from "../Components/Title";
+import ContactSection from "../Components/contactSection";
 
 const Contact = () => {
   return (
     <div className="Contact">
       <Title title={"Contact"} span={"Contact"} />
+      <div className="map-sect"></div>
+      <div className="contact-sect">
+        <ContactSection
+          icon={phone}
+          text1={"+2347033960652"}
+          text2={"(+234) 815 2269 553"}
+        />
+        <ContactSection
+          icon={email}
+          text1={"+2347033960652"}
+          text2={"(+234) 815 2269 553"}
+        />
+        <ContactSection
+          icon={location}
+          text1={"+2347033960652"}
+          text2={"(+234) 815 2269 553"}
+        />
+      </div>
     </div>
   );
 };
