1a027a477c2dd629f758776aeebbefcd365660dc
Kenneth <kenneth.aladi@xische.com>
2021-07-23 16:22:51 +0100

    Added category and menu components

--- src/pages/Portfolios.js
+++ src/pages/Portfolios.js
@@ -1,10 +1,19 @@
 import React from "react";
+import Categories from "../Components/Categories";
+import MenuItems from "../Components/MenuItems";
 import Title from "../Components/Title";
 
 const Portfolios = () => {
   return (
     <div className="Portfolio">
-      <Title title={"Portfolio"} span={"Portfolio"} />
+      <div className="title">
+        <Title title={"Portfolio"} span={"Portfolio"} />
+      </div>
+      <div className="portfolioDetails">
+        <Categories />
+        <MenuItems />
+      </div>
+
     </div>
   );
 };
