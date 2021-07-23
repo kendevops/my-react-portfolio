a3838ef67edfa7e8b4a3472ab012480407a8922c
Kenneth <kenneth.aladi@xische.com>
2021-07-23 15:35:18 +0100

    cAdded portfolio images and created data structure for that

--- src/Components/PortfolioData.js
+++ src/Components/PortfolioData.js
@@ -1,13 +1,92 @@
-import { useState } from "react"
-import React from 'react'
+import previewImage from "../img/view.png"
+import gitIcon from "../img/github.png"
+import recipe from "../img/recipe-App.jpeg"
+import saleswale from "../img/saleswale-site.png"
+import covid19 from "../img/Covid19.png"
+import chrome from "../img/chrome-extention.png"
+import arcade from "../img/arcade.png"
+import movies from "../img/nominate-movies.png"
 
-const portfolios = [
+const Portfolios = [
     {
         id: 1,
-        category: ""
+        title: "Recipe App",
+        category: "JavaScript",
+        image: recipe,
+        previewIcon: previewImage,
+        githubIcon: gitIcon,
+        preview: "https://kendevops.github.io/More-Recipes/",
+        github: "https://github.com/kendevops/More-Recipes",
+
     },
-    {},
-    {},
+    {
+        id: 2,
+        title: "Sales Wale Site",
+        category: "React",
+        image: saleswale,
+        previewIcon: previewImage,
+        githubIcon: gitIcon,
+        preview: "https://sharp-hodgkin-517e19.netlify.app/",
+        github: "https://github.com/kendevops/saleswhale",
+
+    },
+    {
+        id: 3,
+        title: "Covid19 Tracker",
+        category: "React",
+        image: covid19,
+        previewIcon: previewImage,
+        githubIcon: gitIcon,
+        preview: "https://5eb05e21417a7801dac19ba1--ken-covid19-tracker.netlify.app/",
+        github: "https://github.com/kendevops/covid-19-tracker",
+
+    },
+    {
+        id: 4,
+        title: "Real Time Chrome Extention",
+        category: "React",
+        image: chrome,
+        previewIcon: previewImage,
+        githubIcon: gitIcon,
+        preview: "https://kendevops.github.io/JS_Dynamic_Landing_Page/",
+        github: "https://github.com/kendevops/JS_Dynamic_Landing_Page",
+
+    },
+    {
+        id: 5,
+        title: "Arcade Game",
+        category: "React",
+        image: arcade,
+        previewIcon: previewImage,
+        githubIcon: gitIcon,
+        preview: "https://kendevops.github.io/arcade-game/",
+        github: "https://github.com/kendevops/arcade-game",
+
+    },
+    {
+        id: 6,
+        title: "Movies Nomination",
+        category: "React",
+        image: movies,
+        previewIcon: previewImage,
+        githubIcon: gitIcon,
+        preview: "https://the-shoppies-ken.netlify.app/",
+        github: "https://github.com/kendevops/The-Shoppies",
+
+    },
+    // {
+    //     id: 6,
+    //     title: "Covid19 Tracker",
+    //     category: "React",
+    //     image: covid19,
+    //     previewIcon: previewImage,
+    //     githubIcon: gitIcon,
+    //     preview: "https://the-shoppies-ken.netlify.app/",
+    //     github: "https://github.com/kendevops/The-Shoppies",
+
+    // },
+
+    // ["https://kendevops.github.io/url-shorting/ and https://github.com/kendevops/Insure_landing_page", "https://kendevops.github.io/Insure_landing_page/ and https://github.com/kendevops/Insure_landing_page", "https://ken-rock-scissors-paper.netlify.app/ and https://github.com/kendevops/rock-paper-scissors", "https://iqstore.ivyarc.com/ and https://github.com/kendevops/iqstore-frontend"]
 ]
 
-export default PortfolioData
+export default Portfolios;
