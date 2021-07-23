f31dab37deefadaec3e12ee10eb88cd850c5c3b4
Kenneth <kenneth.aladi@xische.com>
2021-07-23 16:28:53 +0100

    added the data from portfolios to portfolio page

--- src/pages/Portfolios.js
+++ src/pages/Portfolios.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import Categories from "../Components/Categories";
 import MenuItems from "../Components/MenuItems";
 import Title from "../Components/Title";
+import Portfolio from "../Components/PortfolioData";
 
 const Portfolios = () => {
+  const [categories, setCategories] = useState(null);
+  const [menuItems, setMenuItems] = useState(Portfolio);
+
   return (
     <div className="Portfolio">
       <div className="title">
         <Title title={"Portfolio"} span={"Portfolio"} />
       </div>
       <div className="portfolioDetails">
-        <Categories />
-        <MenuItems />
+        <Categories categories={categories}/>
+        <MenuItems menuItem={menuItems}/>
       </div>
 
     </div>
