e60d2bda6cad626601781aea8cfed8ce258eb8db
Kenneth Aladi <kennethmichael11@gmail.com>
2024-03-22 16:35:18 +0000

    made modifications on the portfolio project

--- src/Components/PortfolioData.js
+++ src/Components/PortfolioData.js
@@ -1,3 +1,4 @@
+import creon from "../img/Creon.png"
 import previewImage from "../img/view.webp"
 import gitIcon from "../img/github.webp"
 import recipe from "../img/recipe-App.webp"
@@ -9,9 +10,19 @@ import movies from "../img/nominate-movies.webp"
 import landing from "../img/landing.webp"
 
 const PortfolioData = [
-
     {
         id: 1,
+        title: "Creon AI",
+        category: "Nextjs",
+        image: creon,
+        previewIcon: previewImage,
+        githubIcon: gitIcon,
+        preview: "https://creon.vercel.app/",
+        github: "https://github.com/kendevops/creon-test",
+
+    },
+    {
+        id: 2,
         title: "Sales Wale Clone",
         category: "React",
         image: saleswale,
@@ -22,7 +33,7 @@ const PortfolioData = [
 
     },
     {
-        id: 2,
+        id: 3,
         title: "Covid19 Tracker",
         category: "React",
         image: covid19,
@@ -33,7 +44,7 @@ const PortfolioData = [
 
     },
     {
-        id: 3,
+        id: 4,
         title: "Real Time Chrome Extention",
         category: "JavaScript",
         image: chrome,
@@ -44,7 +55,7 @@ const PortfolioData = [
 
     },
     {
-        id: 4,
+        id: 5,
         title: "Recipe App",
         category: "JavaScript",
         image: recipe,
@@ -55,7 +66,7 @@ const PortfolioData = [
 
     },
     {
-        id: 5,
+        id: 6,
         title: "Arcade Game",
         category: "JavaScript",
         image: arcade,
@@ -66,7 +77,7 @@ const PortfolioData = [
 
     },
     {
-        id: 6,
+        id: 7,
         title: "Movies Nomination",
         category: "React",
         image: movies,
@@ -77,7 +88,7 @@ const PortfolioData = [
 
     },
     {
-        id: 7,
+        id: 8,
         title: "Video Landing Page",
         category: "JavaScript",
         image: landing,
